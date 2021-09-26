58c4842dbf750a8c1a0c6538046205e7d4648d9c
William Gotterer <william.gotterer@gmail.com>
2021-09-26 18:20:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.querySelector("form")
+
+  ////Makes the drop down menu
+  let menu = document.createElement("SELECT")
+  let priority = ["Priority Level", "High", "Medium", "Low"];
+  for(let importance of priority){
+  let option = document.createElement("option")
+  option.value = importance
+  option.textContent = `${importance}`
+  menu.appendChild(option)
+  }
+  
+  form.appendChild(menu)
+
+
+ 
+
+
+
+ /////listens for the submission of new task
+  form.addEventListener("submit", function(e) {
+    e.preventDefault() /// prevents url change when submit is clicked
+    let importance = form.querySelector("select").value
+    buildToDo(e.target.children[1].value, importance) ///grabs ahold of the second child in form
+    form.reset() ///resets form after funcion runs
+    
+  })
+ 
+  
+
 });
+
+ 
+
+////makes to do list.
+function buildToDo(todo, importance){
+
+    let li = document.createElement("li")
+    let btn = document.createElement("button")
+    btn.addEventListener("click", handleDelete)
+    btn.textContent = "x"
+    li.textContent = `${todo} `
+    li.appendChild(btn)
+    document.querySelector("#tasks").appendChild(li)
+
+    if(importance === "High"){ /// turn priorities different colrs
+      li.style.color = "red";
+    }if(importance === "Medium"){
+      li.style.color = "pink";
+    }if(importance === "Low"){
+      li.style.color = "blue";
+    }
+
+    
+
+}
+
+function handleDelete(e){
+  
+  e.target.parentNode.remove()
+}
+
